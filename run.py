c343deeb933215dd7ed95d6ce0ff98e245a83470
Jamie Bull <jamie.bull@oco-carbon.com>
2017-03-06 10:50:50 +0100

    Initial site content

--- run.py
+++ run.py
@@ -3,21 +3,29 @@
 from flask import Flask
 from flask import render_template
 
-
-# app = Flask(__name__, static_url_path="", static_folder="static")
 app = Flask(__name__)
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-
 @app.route('/')
 def index():
-#    return app.root_path
     return render_template('index.html')
+
+@app.route('/eventos')
+def eventos():
+    return render_template('eventos.html')
+
+@app.route('/quienes_somos')
+def quienes_somos():
+    return render_template('quienes_somos.html')
+
+@app.route('/como_funciona')
+def como_funciona():
+    return render_template('como_funciona.html')
 
 
 if __name__ == "__main__":
-#    app.run(host='0.0.0.0')
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
+#    app.run(debug=True, host='0.0.0.0')
