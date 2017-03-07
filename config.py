3b64d5b5c08d62f845ff28ce26ce4f9b9d88f2d3
Jamie Bull <jamie.bull@oco-carbon.com>
2017-03-07 09:42:09 +0100

    Make the site easily configurable by other groups using config

--- config.py
+++ config.py
@@ -1,4 +1,15 @@
+# -*- coding: UTF8 -*-
 """
 """
-DEBUG = False
-CITY = "Caceres"
+
+class ProductionConfig(object):
+
+    DEBUG = False
+    CITY = u"Cáceres"
+    GROUP_NAME = u"Sopa CC"
+    FACEBOOK_ID = u"sopacaceres"
+    TWITTER_ID = u"sopa_cc"
+    EMAIL_ID = u"jamiebull1+sopacc@gmail.com"
+    SOUP_FEE = u"5"
+    USER001 = u"Jamie"
+    USER001_BIO = u"Jamie es un inglés, actualmente ubicado en Cáceres, con el deseo de devolver algo a la comunidad a cambio de las buenas cosas que ha recibido."
--- run.py
+++ run.py
@@ -1,25 +1,42 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import os
+
 from flask import Flask
 from flask import render_template
 
+
 app = Flask(__name__)
+
+config = {
+    "development": "instance.config.DevelopmentConfig",
+    "default": "instance.config.DevelopmentConfig",
+    "production": "config.ProductionConfig"
+}
+
+def configure_app(app):
+    config_name = os.getenv('FLASK_CONFIGURATION', 'default')
+    app.config.from_object(config[config_name])  # object-based default configuration
+    app.config.from_pyfile('config.cfg', silent=True)  # instance-folders configuration
+
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/eventos')
 def eventos():
     return render_template('eventos.html')
 
+
 @app.route('/quienes_somos')
 def quienes_somos():
     return render_template('quienes_somos.html')
+
 
 @app.route('/como_funciona')
 def como_funciona():
@@ -27,5 +44,6 @@ def como_funciona():
 
 
 if __name__ == "__main__":
+#    app.run(host='0.0.0.0')
+    configure_app(app)
     app.run(host='0.0.0.0')
-#    app.run(debug=True, host='0.0.0.0')
